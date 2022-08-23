9dcbc312471a330dd248d201ab6fdb11963d6e03
alexannmill <alexannmill@gmail.com>
2022-08-23 22:04:28 +0000

    joining array list, changed min

--- min.js
+++ min.js
@@ -13,7 +13,7 @@ const min = function(numbers) {
   let minNum = numbers[0];
   for (let i = 0; i < numbers.length; i++) {
       if (numbers[i] < minNum){
-        minNum = numbers[i]
+        minNum = numbers[i];
       }
     }
   return minNum;
