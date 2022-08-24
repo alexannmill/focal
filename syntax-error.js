97c4dd844f9c2de00ca32be5199e92740a18f60b
alexannmill <alexannmill@gmail.com>
2022-08-24 00:18:46 +0000

    addinng files; syntaxerrors, average

---
+++ syntax-error.js
@@ -0,0 +1,19 @@
+// var rank = "Imperator";
+// var name = "Furiosa";
+
+// console.log(rank, name);
+
+// if (5 > 10) {
+//   console.log("Impossible!");
+
+// console.log("Phew, logical fallacies avoided.");
+
+
+// var firstName = "Jane";
+// var lastName = "Doe";
+
+// console.log(firstName, lasName);
+
+var favouriteMeal = "BREAKFAST";
+
+console.log(favouriteMeal.toLower());
